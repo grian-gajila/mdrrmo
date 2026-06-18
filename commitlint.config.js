6596f7fcfb3ff11f0bd76802166bed5e5195d4d8
Grian Gajila <griangajila2@gmail.com>
2026-06-18 18:52:26 +0800

    feat: init next project

---
+++ commitlint.config.js
@@ -0,0 +1,17 @@
+const config = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      ['feat', 'fix', 'chore', 'docs', 'style', 'refactor', 'test'],
+    ],
+    'subject-case': [0],
+    'scope-case': [2, 'always', 'snake-case'],
+    'subject-max-length': [2, 'always', 72],
+    'body-leading-blank': [1, 'always'],
+    'footer-leading-blank': [1, 'always'],
+  },
+};
+
+export default config;
